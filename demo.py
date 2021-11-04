08faf1126b7d6735ecfdc2a40d6eb0751b866e8d
권태윤 <kwontaeyoon@192.168.nate.com>
2021-11-04 18:05:10 +0900

    modify demo

--- demo.py
+++ demo.py
@@ -1,25 +1,59 @@
 import os
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
 from torchvision import transforms
 from torchvision.transforms import ToTensor
 from PIL import Image
 
-subject = str(input("input image path"))
+img_path1 = "./data/clean_yalefaces/subject0"
+img_path2 = "./data/clean_yalefaces/subject"
 
-img = Image.open(subject)
+def main():
+    subject = str(input("input image path\n"))
 
-trans = transforms.Compose([
+    img = Image.open(subject)
+    origin_img = np.array(img)
+
+    trans = transforms.Compose([
                             transforms.Resize((200,200)), 
                             transforms.ToTensor(),
                             transforms.Normalize([0.5],[0.5])
                            ])
-img = trans(img)
-img = img.unsqueeze(0)
+    img = trans(img)
+    img = img.unsqueeze(0)
 
-model = torch.load("./model/CNN_model.pt")
-output = model(img)
-result = torch.max(output,1)
+    model = torch.load("./model/CNN_model.pt")
+    output = model(img)
+    result = torch.max(output,1)
 
-label_index = int(result.indices[0]) + 1
+    label_index = int(result.indices[0]) + 1
 
-print("Predicted subject is : subject%d"%(label_index))
+    if(label_index < 10) :
+        path = img_path1
+    else :
+        path = img_path2
+
+    p_img = Image.open(path + str(label_index)+"_normal.jpg")
+    predict_img = np.array(p_img)
+
+    # plot
+    fig = plt.figure()
+
+    fst = fig.add_subplot(1,2,1)
+    fst.imshow(origin_img,cmap = plt.get_cmap("gray"))
+    fst.set_title("Input Subject")
+    fst.axis("off")
+
+    snd = fig.add_subplot(1,2,2)
+    snd.imshow(predict_img,cmap=plt.get_cmap("gray"))
+    snd.set_title("Predicted Subject" + str(label_index))
+    snd.axis("off")
+
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+    main()
